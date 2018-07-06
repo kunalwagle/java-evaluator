be8a975cd4ab0eb37bb47f50e9bb523d3c1605f5
Kunal Wagle <kmw13@ic.ac.uk>
2018-07-06 11:17:16 +0100

    Fully tested java evaluator with unit tests

--- src/main/java/controllers/EvaluationController.java
+++ src/main/java/controllers/EvaluationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,28 +20,42 @@ import java.util.List;
 @RestController
 public class EvaluationController {
 
+    private List<Test> tests;
+
+    public EvaluationController() {
+        this.tests = generateTests();
+    }
+
+    public EvaluationController(List<Test> tests) {
+        this.tests = tests;
+    }
+
     @RequestMapping(path="/evaluate", method= RequestMethod.POST)
     public TestResult evaluate(@RequestBody String input) {
 
-        Class<?> compiledClass = compileInput(input);
+        List<CompletedTest> completedTests = new ArrayList<>();
+
+        Class<?> compiledClass = null;
+        try {
+            compiledClass = compileInput(input);
+        } catch (Exception e) {
+            completedTests.add(new CompletedTest(tests.get(0), false, e.getMessage()));
+        }
 
         boolean successfullyCompiled = compiledClass != null;
 
-        if (!successfullyCompiled) {
-            return null;
-        }
+        if (successfullyCompiled) {
 
-        List<Test> tests = generateTests();
-        List<CompletedTest> completedTests = new ArrayList<>();
+            assert tests != null;
 
-        assert tests != null;
-
-        for (Test test : tests) {
-            CompletedTest completedTest = runTest(compiledClass, test);
-            completedTests.add(completedTest);
-            if (!completedTest.isPassed()) {
-                break;
+            for (Test test : tests) {
+                CompletedTest completedTest = runTest(compiledClass, test);
+                completedTests.add(completedTest);
+                if (!completedTest.isPassed()) {
+                    break;
+                }
             }
+
         }
 
         return new TestResult(completedTests, tests.size());
@@ -51,40 +66,36 @@ public class EvaluationController {
             Object answer = compiledClass.newInstance();
             int[] response = (int[]) compiledClass.getMethods()[0].invoke(answer, test.getFirstInput(), test.getSecondInput(), test.getThirdInput()); ///(test.getFirstInput(), test.getSecondInput(), test.getThirdInput());
             if (Arrays.equals(test.getExpected(), response)) {
-                return new CompletedTest(test, true, "components.Test Passed");
+                return new CompletedTest(test, true, "Test Passed");
             } else {
-                return new CompletedTest(test, false, "components.Test Failed");
+                return new CompletedTest(test, false, "Expected " + Arrays.toString(test.getExpected()) + ", Got " + Arrays.toString(response));
             }
-        } catch (Exception e) {
+        } catch (InvocationTargetException e) {
+            Throwable exception = e.getTargetException();
             if (test.isExceptionExpected()) {
-                if (test.getExceptionText().equals(e.getCause().toString())) {
-                    return new CompletedTest(test, true, "components.Test Passed");
+                if (test.getExceptionText().equals(exception.toString())) {
+                    return new CompletedTest(test, true, "Test Passed");
                 }
+                return new CompletedTest(test, false, "Expected exception with message: " + test.getExceptionText() + ", but got message: " + exception.toString());
             }
-            return new CompletedTest(test, false, e.getMessage());
+            return new CompletedTest(test, false, "Got an exception. Message: " + exception.toString());
+        } catch (Exception e) {
+            return new CompletedTest(test, false, "Got an exception. Message: " + e.getMessage());
         }
     }
 
     private List<Test> generateTests() {
         List<Test> tests = new ArrayList<>();
         tests.add(new Test("Test 1", Category.BASIC, 10, 20, 30, new int[]{10, 20, 30}, false, ""));
-        tests.add(new Test("Test 2", Category.BASIC, 10, 20, 30, new int[]{10, 20, 30}, false, ""));
-        tests.add(new Test("Test 3", Category.MEDIUM, 10, 20, 30, new int[]{10, 20, 30}, false, ""));
-        tests.add(new Test("Test 4", Category.DIFFICULT, 10, 20, 30, new int[]{10, 20, 30}, false, ""));
+        tests.add(new Test("Test 2", Category.BASIC, 10, 20, 30, new int[]{10, 20, 40}, false, ""));
+        tests.add(new Test("Test 3", Category.MEDIUM, 10, 20, 30, new int[]{10, 20, 50}, false, ""));
+        tests.add(new Test("Test 4", Category.DIFFICULT, 10, 20, 30, new int[]{10, 20, 60}, false, ""));
 
         return tests;
     }
 
-    private Class<?> compileInput(String input) {
-
-        Class<?> compiledClass = null;
-        try {
-            compiledClass = InMemoryJavaCompiler.newInstance().compile("components.Answer", input);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return compiledClass;
+    private Class<?> compileInput(String input) throws Exception {
+        return InMemoryJavaCompiler.newInstance().compile("components.Answer", input);
     }
 
 }
--- src/test/java/controllers/EvaluationControllerTest.java
+++ src/test/java/controllers/EvaluationControllerTest.java
@@ -1,11 +1,161 @@
 package controllers;
 
+import components.Category;
+import components.CompletedTest;
+import components.TestResult;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class EvaluationControllerTest {
 
-    @Test
-    public void evaluate() {
-        assert(true);
+    private List<components.Test> generateTests() {
+        List<components.Test> tests = new ArrayList<>();
+        tests.add(new components.Test("Test 1", Category.BASIC, 10, 20, 30, new int[]{10, 20, 30}, false, ""));
+        tests.add(new components.Test("Test 2", Category.BASIC, 10, 20, 30, new int[]{10, 20, 40}, false, ""));
+        return tests;
     }
+
+    @Test
+    public void compilationErrorGetsReturned() {
+        String code = "package components;\n" +
+                "\n" +
+                "public class Answer {\n" +
+                "\n" +
+                "    public int[] answer(int input1, int input2, int input3) {\n" +
+                "        return new int[]{10, 20, 30}\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+        TestResult testResult = new EvaluationController(generateTests()).evaluate(code);
+        Map<String, List<CompletedTest>> map = testResult.getCompletedTests();
+        List<CompletedTest> completedTests = map.get(Category.BASIC.getType());
+        CompletedTest completedTest = completedTests.get(0);
+        Assert.assertEquals(1, map.size());
+        Assert.assertEquals(1, completedTests.size());
+        Assert.assertEquals("Unable to compile the source\n" +
+                "[kind=ERROR, line=6, message=';' expected]", completedTest.getMessage());
+    }
+
+    @Test
+    public void compilesAndRunsTestsUntilFailure() {
+        String code = "package components;\n" +
+                "\n" +
+                "public class Answer {\n" +
+                "\n" +
+                "    public int[] answer(int input1, int input2, int input3) {\n" +
+                "        return new int[]{10, 20, 30};\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+        TestResult testResult = new EvaluationController(generateTests()).evaluate(code);
+        Map<String, List<CompletedTest>> map = testResult.getCompletedTests();
+        List<CompletedTest> completedTests = map.get(Category.BASIC.getType());
+        CompletedTest completedTest1 = completedTests.get(0);
+        CompletedTest completedTest2 = completedTests.get(1);
+        Assert.assertEquals(1, map.size());
+        Assert.assertEquals(2, completedTests.size());
+        Assert.assertEquals(completedTest1.isPassed(), true);
+        Assert.assertEquals(completedTest1.getMessage(), "Test Passed");
+        Assert.assertEquals(completedTest2.isPassed(), false);
+    }
+
+    @Test
+    public void givesSensibleErrorMessage() {
+        String code = "package components;\n" +
+                "\n" +
+                "public class Answer {\n" +
+                "\n" +
+                "    public int[] answer(int input1, int input2, int input3) {\n" +
+                "        return new int[]{10, 20, 40};\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+        TestResult testResult = new EvaluationController(generateTests()).evaluate(code);
+        Map<String, List<CompletedTest>> map = testResult.getCompletedTests();
+        List<CompletedTest> completedTests = map.get(Category.BASIC.getType());
+        CompletedTest completedTest = completedTests.get(0);
+        Assert.assertEquals(1, map.size());
+        Assert.assertEquals(1, completedTests.size());
+        Assert.assertEquals(completedTest.isPassed(), false);
+        Assert.assertEquals(completedTest.getMessage(), "Expected [10, 20, 30], Got [10, 20, 40]");
+    }
+
+    @Test
+    public void passesTestWhenExceptionIsThrownCorrectly() {
+        String code = "package components;\n" +
+                "\n" +
+                "public class Answer {\n" +
+                "\n" +
+                "    public int[] answer(int input1, int input2, int input3) {\n" +
+                "        throw new NullPointerException(\"Something is null\");\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+        components.Test test = new components.Test("Exception", Category.BASIC, 0, 0, 0, null, true, "java.lang.NullPointerException: Something is null");
+        List<components.Test> tests = new ArrayList<>();
+        tests.add(test);
+        TestResult testResult = new EvaluationController(tests).evaluate(code);
+        Map<String, List<CompletedTest>> map = testResult.getCompletedTests();
+        List<CompletedTest> completedTests = map.get(Category.BASIC.getType());
+        CompletedTest completedTest = completedTests.get(0);
+        Assert.assertEquals(1, map.size());
+        Assert.assertEquals(1, completedTests.size());
+        Assert.assertEquals(completedTest.isPassed(), true);
+        Assert.assertEquals(completedTest.getMessage(), "Test Passed");
+    }
+
+    @Test
+    public void failsTestWhenExceptionIsThrownIncorrectly() {
+        String code = "package components;\n" +
+                "\n" +
+                "public class Answer {\n" +
+                "\n" +
+                "    public int[] answer(int input1, int input2, int input3) {\n" +
+                "        int[] arr = new int[]{2};\n" +
+                "        int x = arr[42];\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+        TestResult testResult = new EvaluationController(generateTests()).evaluate(code);
+        Map<String, List<CompletedTest>> map = testResult.getCompletedTests();
+        List<CompletedTest> completedTests = map.get(Category.BASIC.getType());
+        CompletedTest completedTest = completedTests.get(0);
+        Assert.assertEquals(1, map.size());
+        Assert.assertEquals(1, completedTests.size());
+        Assert.assertEquals(completedTest.isPassed(), false);
+        Assert.assertEquals(completedTest.getMessage(), "Got an exception. Message: java.lang.ArrayIndexOutOfBoundsException: 42");
+    }
+
+    @Test
+    public void failsTestWhenWrongExceptionIsThrown() {
+        String code = "package components;\n" +
+                "\n" +
+                "public class Answer {\n" +
+                "\n" +
+                "    public int[] answer(int input1, int input2, int input3) {\n" +
+                "        int[] arr = new int[]{2};\n" +
+                "        int x = arr[42];\n" +
+                "        return null;\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+        components.Test test = new components.Test("Exception", Category.BASIC, 0, 0, 0, null, true, "java.lang.NullPointerException: Something is null");
+        List<components.Test> tests = new ArrayList<>();
+        tests.add(test);
+        TestResult testResult = new EvaluationController(tests).evaluate(code);
+        Map<String, List<CompletedTest>> map = testResult.getCompletedTests();
+        List<CompletedTest> completedTests = map.get(Category.BASIC.getType());
+        CompletedTest completedTest = completedTests.get(0);
+        Assert.assertEquals(1, map.size());
+        Assert.assertEquals(1, completedTests.size());
+        Assert.assertEquals(completedTest.isPassed(), false);
+        Assert.assertEquals(completedTest.getMessage(), "Expected exception with message: java.lang.NullPointerException: Something is null, but got message: java.lang.ArrayIndexOutOfBoundsException: 42");
+    }
+
+
 }
