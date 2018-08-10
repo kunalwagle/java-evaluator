74aed95d9eb5ba11670b8f0ee58deed829552fb0
CX-16 <dimarammfire@CX-2.local>
2018-08-10 11:00:27 +0100

    [dimasquest] Changed the random num/char generator to a better definition.

--- src/main/java/controllers/RandomUtils.java
+++ src/main/java/controllers/RandomUtils.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class RandomUtils {
 
-    public Integer[] randomizeSequence(Integer[] array, int lower, int upper, int length) {
+    public Integer[] randomizeSequence(Integer[] array) {
         List<Integer> asList = Arrays.asList(array);
         Collections.shuffle(asList);
         return (Integer[]) asList.toArray();
@@ -19,8 +19,7 @@ public class RandomUtils {
     }
 
     private int getRandomInt(int min, int max) {
-        return (int) Math.floor(Math.random() * (Math.floor(max)
-                - Math.ceil(min) + 1) + Math.ceil(min) + 1);
+        return new Random().nextInt(max - min + 1) + min;
     }
 //    This is a mess, ive no idea what input type is
     public String[] randomlyRemoveField(String[][] input, String field) {
@@ -30,13 +29,10 @@ public class RandomUtils {
         return (String[]) inputAsList.toArray();
     }
 
-    public String getSorterString() {
-        return null;
-    }
-
     public char getRandomChar() {
-        int upperCase = (int) Math.random();
-        return (char) (new Random().nextInt(26) + 'a');
+        int upperCase = getRandomInt(1, 2);
+        char result = (char) (new Random().nextInt(26) + 'a');
+        return (upperCase % 2 == 0) ? result : Character.toUpperCase(result);
     }
 
     private String[] removeStringFromArray(String[] array, String toBeRemoved) {
